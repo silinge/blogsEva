9ae144e444d566280620d6c07fc0c8915ebe8537
william <gatesyyy@gmail.com>
2026-01-25 21:17:03 +0800

    fix: explicit env path force update

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+// Force git update
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const express = require('express');
 const session = require('express-session');
